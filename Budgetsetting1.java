8fa50d0ff9310f8abee978cace3ecdc5fc1d93d2
Bhavy14 <bhavy.14.66@gmail.com>
2024-10-07 23:31:40 +0530

    Update Budgetsetting1.java

--- Budgetsetting1.java
+++ Budgetsetting1.java
@@ -1,13 +1,10 @@
-//package directory
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-/*Users can set monthly budgets for different expense categories to avoid overspending. */
 public class Budgetsetting1 {
     public double m_exp;
     public String cat;
     public static Map<String, Double> expcat = new HashMap<>();
-    //public static ArrayList<String> arrlist = new ArrayList<>();
 
     public void set(double mexp) {
         this.m_exp = mexp;
@@ -21,7 +18,7 @@ public class Budgetsetting1 {
         try {
             FileReader f1 = new FileReader("Budget.txt");
             BufferedReader bfr = new BufferedReader(f1);
-            String str = "";
+            String str;
             while ((str = bfr.readLine()) != null) {
                 System.out.println(str);
             }
@@ -30,63 +27,24 @@ public class Budgetsetting1 {
         } catch (Exception e) {
             System.out.println(e);
         }
-
     }
-/* 
-    public static void show() {
-        try {
-            FileReader f1 = new FileReader("hello.txt");
-            BufferedReader bfr = new BufferedReader(f1);
-            String str = "";
-            while ((str = bfr.readLine()) != null) {
-                System.out.println(str);
-                arrlist.add(str);
-            }
-            bfr.close();
-            f1.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
 
-    }
-*/
     public void setmonthlybudget() {
         Scanner sc = new Scanner(System.in);
         String cate;
-
-        //show();
 
         try {
             FileWriter f2 = new FileWriter("Budget.txt", true);
             System.out.println("Enter category to set a Budget = ");
             cate = sc.next();
-            System.out.println("Enter Bugdet Amount for " + cate + " =");
+            System.out.println("Enter Budget Amount for " + cate + " =");
             m_exp = sc.nextDouble();
             expcat.put(cate, m_exp);
-            for (Map.Entry<String, Double> entry : expcat.entrySet()) {
-                f2.write(entry.getKey() + "=" + entry.getValue() + "\n");
-            }
-            /*if (arrlist.contains(cate)) {
-                System.out.println("Set montly bugdet for " + cate + " =");
-                m_exp = sc.nextDouble();
-                expcat.put(cate, m_exp);
-                for (Map.Entry<String, Double> entry : expcat.entrySet()) {
-                    f2.write(entry.getKey() + "=" + entry.getValue() + "\n");
-                }
-
-            } else {
-                System.out.println("Invalid category");
-            }*/
+            f2.write(cate + "=" + m_exp + "\n");
             f2.close();
             show2();
-        } 
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
-
-    /*public static void main(String args[]) {
-        Budgetsetting1 b = new Budgetsetting1();
-        b.setmonthlybudget();
-    }*/
 }
